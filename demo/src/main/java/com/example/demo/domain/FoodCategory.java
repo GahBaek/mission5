c5af678b34710201fa0bd23b91b01cf2920661cb
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-04-27 20:15:55 +0900

    feat: FoodCategory 추가

--- demo/src/main/java/com/example/demo/domain/FoodCategory.java
+++ demo/src/main/java/com/example/demo/domain/FoodCategory.java
@@ -1,16 +1,23 @@
 package com.example.demo.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity(name = "food_category")
-public class food_category extends BaseEntity{
+public class FoodCategory extends BaseEntity{
     String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<MemberPrefer> memberPrefers = new ArrayList<>();
 }
