03110b711892398f8bea57a454fcfdddbac82e75
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-04-27 20:39:23 +0900

    feat: modifying 추가

--- demo/src/main/java/com/example/demo/repository/MemberMissionRepository.java
+++ demo/src/main/java/com/example/demo/repository/MemberMissionRepository.java
@@ -2,9 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.MemberMission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface MemberMissionRepository extends JpaRepository<MemberMission, UUID> {
-    void deleteByMemberId(UUID memberId);
-}
+    @Modifying
+    void deleteByMemberId(UUID memberId);}
--- demo/src/main/java/com/example/demo/repository/ReviewRepository.java
+++ demo/src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+    @Modifying
     void detachMember(UUID memberId);
 }
