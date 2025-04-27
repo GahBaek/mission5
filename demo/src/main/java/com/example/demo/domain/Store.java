13a6f18983a54e735478d87271d6be4585e3fb82
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-04-27 20:17:58 +0900

    Store 추가

--- demo/src/main/java/com/example/demo/domain/Store.java
+++ demo/src/main/java/com/example/demo/domain/Store.java
@@ -1,18 +1,34 @@
-package com.example.umcproject.domain;
+package com.example.demo.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity(name = "store")
-public class store extends BaseEntity{
+public class Store extends BaseEntity{
     String name;
     String address;
     float score;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "store")
+    private List<Mission> missions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store")
+    private List<Review> reviews = new ArrayList<>();
 }
