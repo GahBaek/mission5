c590c29879c00140dad937c0ffc02a4fe43f9359
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-04-27 20:17:19 +0900

    Region 추가

--- demo/src/main/java/com/example/demo/domain/Region.java
+++ demo/src/main/java/com/example/demo/domain/Region.java
@@ -1,19 +1,23 @@
-package com.example.umcproject.domain;
+package com.example.demo.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.LastModifiedDate;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity(name = "region")
-public class region extends BaseEntity{
+public class Region extends BaseEntity{
     String name;
+
+    @OneToMany(mappedBy = "region")
+    private List<Store> stores = new ArrayList<>();
 }
