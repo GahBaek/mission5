ba814f9029e291d0eb9afa7626e858d258b9e0bb
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro-3.local>
2025-04-27 20:17:04 +0900

    mission 추가

--- demo/src/main/java/com/example/demo/domain/Mission.java
+++ demo/src/main/java/com/example/demo/domain/Mission.java
@@ -1,22 +1,32 @@
-package com.example.umcproject.domain;
+package com.example.demo.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity(name = "mission")
-public class mission extends BaseEntity{
+public class Mission extends BaseEntity{
     int reward;
     LocalDateTime deadline;
     String mission_spec;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "mission")
+    private List<MemberMission> memberMissions = new ArrayList<>();
 }
